3aa371414ace0cb2e7b2715815e64a3766ea14f4
ld1001 <776537387@qq.com>
2020-04-27 03:04:09 +0800

    6-8token配置

---
+++ imooc-security-app/src/main/java/com/imooc/security/app/ImoocAuthorizationServerConfig.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package com.imooc.security.app;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.builders.InMemoryClientDetailsServiceBuilder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+import com.imooc.security.core.properties.OAuth2ClientProperties;
+import com.imooc.security.core.properties.SecurityProperties;
+
+/**
+ * @author zhailiang
+ *
+ */
+@Configuration
+@EnableAuthorizationServer
+public class ImoocAuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private TokenStore tokenStore;
+
+	@Autowired
+	private SecurityProperties securityProperties;
+
+	// endpoints是处理token的入口，所以设置相关配置要在这里设置
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.tokenStore(tokenStore)
+				.authenticationManager(authenticationManager)
+				.userDetailsService(userDetailsService);
+	}
+
+	// 第三方应用相关配置，重写了这个方法，下面的配置就不起作用了
+	// 配置OAuth的clientId
+	// security.oauth2.client.clientId=imooc
+	// security.oauth2.client.clientSecret = imoocsecret
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		InMemoryClientDetailsServiceBuilder builder = clients.inMemory();
+		if (ArrayUtils.isNotEmpty(securityProperties.getOauth2().getClients())) {
+			for (OAuth2ClientProperties client : securityProperties.getOauth2().getClients()) {
+				builder.withClient(client.getClientId())
+						.secret(client.getClientSecret())
+						.authorizedGrantTypes("refresh_token", "authorization_code", "password")
+						.accessTokenValiditySeconds(client.getAccessTokenValidateSeconds())
+						.refreshTokenValiditySeconds(2592000)
+						.scopes("all");
+			}
+		}
+	}
+
+}
